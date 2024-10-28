5fbbccbff439154eeaee2c9b571ccb5a1d69f64f
Wassim Salam <97744975+wassimsalam01@users.noreply.github.com>
2024-10-28 20:39:59 +0100

    Update DMRichR-Testing.R

--- r-scripts/DMRichR-Testing.R
+++ r-scripts/DMRichR-Testing.R
@@ -1,19 +1,24 @@
+# Setup
+setwd("/path/to/cytosine-reports")
+# In working directory must be present:
+# BSgenome, TxDb and org.db packages (.tar.gz) (if not already installed)
+# sample_info.xlsx
+# Cytosine Reports (.CpG_report.txt.gz)
+# CGI-Dpulex.txt
+
+# Package installation
 devtools::install_github("wassimsalam01/DMRichR")
 devtools::install_github("wassimsalam01/annotatr", force = TRUE)
 devtools::install_github("wassimsalam01/dmrseq", force = TRUE)
 devtools::install_github("wassimsalam01/ChIPseeker", force = TRUE)
-setwd("/path/to/cytosine-reports")
-# In working directory must be present:
-# BSgenome seed file (if compiling BSgenome from scratch)
-# sample_info.xlsx
-# Cytosine Reports
-# CGI-Dpulex.txt
+
+# Execution
 DMRichR::DM.R(genome = "Dpulex",
               coverage = 1,
               perGroup = 0.6,
               minCs = 3,
-              maxPerms = 1,
-              maxBlockPerms = 1,
+              maxPerms = 10,
+              maxBlockPerms = 10,
               cutoff = 0.05,
               testCovariate = "Group",
               cores = 1,
